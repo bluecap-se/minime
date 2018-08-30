e51028b25d05667b159b0fbe6a6ab74cbcf25734
Andrej Babic <hello@bluecap.se>
2018-08-30 10:44:32 +0200

    Feature: Cache url

--- app/minime/views.py
+++ app/minime/views.py
@@ -1,26 +1,33 @@
-
 import json
-import random
-import string
 
+from django.core.cache import cache
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
-from .forms import ShortenURLForm
-from .models import Url
+from . import forms
+from . import models
+from . import utils
+
+
+cache_tmpl = 'short:url:{}'
+cache_ttl = 60 * 60 * 24 * 7  # 7 days
 
 
 def index(request):
-    return render(request, 'index.html', {'form': ShortenURLForm})
+    return render(request, 'index.html', {'form': forms.ShortenURLForm})
 
 
 def redirect(request, hash):
-    url = get_object_or_404(Url, hash=hash)  # gets object, if not found returns 404 error
-    url.count += 1
-    url.save()
 
-    return HttpResponseRedirect(url.url)
+    url = cache.get(cache_tmpl.format(hash))
+
+    if not url:
+        urlobj = get_object_or_404(models.Url, hash=hash)  # gets object, if not found returns 404 error
+        url = urlobj.url
+        cache.set('short:url:{}'.format(hash), url, cache_ttl)
+
+    return HttpResponseRedirect(url)
 
 
 def shorten_url(request):
@@ -28,31 +35,14 @@ def shorten_url(request):
     url = request.POST.get('url', '')
 
     if url:
-        hash = get_hash()
-        b = Url(url=url, hash=hash)
+        hash = utils.get_hash()
+        b = models.Url(url=url, hash=hash)
         b.save()
+
+        cache.set(cache_tmpl.format(hash), url, cache_ttl)
+        cache.set('hash:taken:{}'.format(hash), 1, None)
 
         response_data = dict(url='{}/{}'.format(settings.SITE_URL, hash))
         return HttpResponse(json.dumps(response_data), content_type='application/json')
 
     return HttpResponse(json.dumps({'error': 'error occurs'}), content_type='application/json')
-
-
-def get_hash():
-    """
-    Creates a unique hash
-
-    TODO: Check hash in redis
-
-    :return: Unique hash string
-    """
-    length = 6
-    char = string.ascii_uppercase + string.digits + string.ascii_lowercase
-
-    # Generate a new ID, until one is found that is unique
-    while True:
-        hash = ''.join(random.choice(char) for x in range(length))
-        try:
-            Url.objects.get(hash=hash)
-        except:
-            return hash
